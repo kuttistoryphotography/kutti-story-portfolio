d4875f27c3b6d56c64384bae910a4fc3d48f0523
Kuttistory <Kuttistoryphotography@gmail.com>
2026-07-31 16:09:07 +0530

    Add sitemap and robots

--- components/BookingCTA.tsx
+++ components/BookingCTA.tsx
@@ -1,48 +1,80 @@
 import Link from "next/link";
+import { Calendar, MessageCircle } from "lucide-react";
 
 export default function BookingCTA() {
   return (
     <section className="bg-white pt-18 pb-32">
+      <div className="h-5 bg-white-500"></div> 
       
-      <div className="mx-auto max-w-5xl px-3">
+      <div className="mt-8 flex flex-col gap-4 px-4 md:px-0 md:flex-row md:justify-center">
 
-        <div className="rounded-[32px] bg-[#849669] px-10 py-20 md:px-20 md:py-12 text-center shadow-[0_30px_80px_rgba(0,0,0,0.15)]">
+        <div className="mx-auto max-w-4xl rounded-[32px] bg-[#849669] px-6 py-14 md:px-16 md:py-16 text-center shadow-[0_30px_80px_rgba(0,0,0,0.15)]">
 
           <p className="mt-4 uppercase tracking-[8px] text-white/80 text-sm">
             Ready to Begin?
           </p>
 
-          <h2 className="mt-3 font-heading text-4xl md:text-5xl font-light leading-tight text-white">
+          <h2 className="mt-3 font-heading text-3xl md:text-5xl font-light leading-tight text-white">
             Let's Tell Your Story
           </h2>
 
-          <p className="mx-auto mt-8 max-w-3xl text-lg leading-8 text-white/80">
-            Every celebration deserves to be remembered beautifully.
-            We'd be honoured to capture your story with timeless
-            photographs and cinematic films.
-          </p>
+          <div className="mx-auto max-w-4xl">
 
-          <div className="mt-4 flex flex-col justify-center gap-2 sm:flex-row">
+            <p className="mt-6 text-base md:text-lg leading-8 text-white/90">
+              Every celebration deserves to be remembered beautifully.
+              We'd be honoured to capture your story with timeless
+              photographs and cinematic films.
+            </p>
+          
+          </div>
 
-          <a
-            href="https://www.kuttistoryphotography.com/contact-us"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex h-11 items-center justify-center rounded-full border border-white px-10 text-base font-medium text-white transition-all duration-300 hover:-translate-y-1 hover:bg-white hover:text-[#849669]"
-          >
-            Book Your Session
-          </a>
+          {/* Desktop Buttons */}
+          <div className="hidden md:flex justify-center gap-4 mt-10">
 
-          <a
-            href="https://wa.me/919342013600"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex h-11 items-center justify-center rounded-full border border-white px-10 text-base font-medium text-white transition-all duration-300 hover:-translate-y-1 hover:bg-white hover:text-[#849669]"
-          >
-            Chat on WhatsApp
-          </a>
+            <Link
+              href="/contact-us"
+              className="flex items-center gap-2 rounded-full border border-white px-10 py-4 font-semibold text-white transition-all duration-300 hover:bg-white hover:text-[#849669]"
+            >
+              <Calendar className="h-5 w-5" />
+              Book Your Session
+            </Link>
 
-        </div>
+            <a
+              href="https://wa.me/919342013600"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2 rounded-full border border-white px-10 py-4 font-semibold text-white transition-all duration-300 hover:bg-white hover:text-[#849669]"
+            >
+              <MessageCircle className="h-5 w-5" />
+              Chat on WhatsApp
+            </a>
+            
+          </div>
+          {/* gap*/}
+          <div className="h-4 bg-white-500"></div> 
+
+          {/* Mobile Buttons */}
+          <div className="mt-10 flex flex-col items-center gap-4 md:hidden">
+
+            <Link
+              href="/contact-us"
+              className="flex h-14 w-55 items-center justify-center gap-2 rounded-full border border-white text-white font-semibold"
+            >
+              <Calendar className="h-5 w-5" />
+              Book Your Session
+            </Link>
+
+            <a
+              href="https://wa.me/919342013600"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex h-14 w-55 items-center justify-center gap-2 rounded-full border border-white text-white font-semibold"
+            >
+              <MessageCircle className="h-4 w-5" />
+              Chat on WhatsApp
+            </a>
+            <div className="h-2 bg-white-500"></div> 
+          </div>
 
         </div>
 
